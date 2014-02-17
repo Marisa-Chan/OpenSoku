e32ccb0c3a14edcb59485855ac30e9fd4cc15988
Marisa-Chan <thunder_8888@mail.ru>
2014-02-17 11:57:28 +0700

    Implement alice func10 code

--- src/chars/alice.h
+++ src/chars/alice.h
@@ -4,6 +4,16 @@
 class char_alice: public char_c
 {
     public:
+
+    int16_t field_890[4];
+     //CF_SP_DOLL_COUNT 0x890 // short
+     //CF_DOLL_COUNT 0x892 // short
+    int16_t field_898;
+    int16_t field_89A;
+    float field_89C[4]; // Dolls X
+    float field_8AC[4]; // Dolls Y
+    int8_t field_8BC[5];
+
     char_alice(inp_ab *func,uint8_t pal = 0);
 
     void func10();
